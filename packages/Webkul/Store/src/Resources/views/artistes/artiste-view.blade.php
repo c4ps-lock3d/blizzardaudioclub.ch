15ebcdfea44af7bcb8fb68284bc9bb0a3c29f4ad
c4ps-lock3d <nicolas.perrenoud@gmail.com>
2024-11-25 17:55:15 +0100

    update imagecache.php and artiste-view.blade.php

--- packages/Webkul/Store/src/Resources/views/artistes/artiste-view.blade.php
+++ packages/Webkul/Store/src/Resources/views/artistes/artiste-view.blade.php
@@ -56,8 +56,21 @@
                 @endforeach
             @endforeach
         </div>
+        @php
+                $arr = array();
+                foreach($artistes->products as $product){
+                    foreach ($product->videoclips as $videoclip) {
+                        $arr[] = $videoclip->youtubetoken;
+                    }
+                }
+                $unique_data = array_unique($arr);
+                foreach($unique_data as $key => $val) {
+                    if ($key === array_key_first($unique_data)) {
+                            echo "<h1 class ='text-2xl mt-[34px] mb-[18px]'>VIDÉOCLIPS</h1>";
 
-        <h1 class ="text-2xl mt-[34px] mb-[18px]">VIDÉOCLIPS</h1>
+                    }
+                }
+            @endphp   
 
         <div class="grid grid-cols-3 gap-6 max-1060:grid-cols-2 max-md:justify-items-center max-md:gap-x-4">
             @php
@@ -68,8 +81,8 @@
                     }
                 }
                 $unique_data = array_unique($arr);
-                foreach($unique_data as $val) {
-                    echo "<lite-youtube class='rounded-lg border border-black' autoload videoid='".$val."'></lite-youtube>";
+                foreach($unique_data as $key => $val) {
+                    echo "<div><lite-youtube class='rounded-lg border border-black' autoload videoid='".$val."'></lite-youtube></div>";
                 }
             @endphp        
         </div>
