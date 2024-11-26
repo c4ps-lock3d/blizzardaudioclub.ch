3e1ae888b2c935737a8241458addd65d893e1680
c4ps-lock3d <nicolas.perrenoud@gmail.com>
2024-11-26 22:01:01 +0100

    update imagecache.php and artiste-view.blade.php

--- packages/Webkul/Store/src/Resources/views/artistes/artiste-view.blade.php
+++ packages/Webkul/Store/src/Resources/views/artistes/artiste-view.blade.php
@@ -57,16 +57,16 @@
         </div>
 
         <div class="grid grid-cols-5 gap-12 max-1060:grid-cols-1 max-md:gap-x-4">
-            @if($count_products === 2)
+            @if($count_products <= 2)
                 <div class="col-span-2">
             @elseif($count_products >= 3)
                 <div class="col-span-3">
             @endif
                 <h1 class ="border-b text-2xl mt-[34px] mb-[18px]">PRODUITS RELATIFS</h1>
-                @if($count_products === 2)
+                @if($count_products <= 2)
                     <div class="grid grid-cols-2 gap-6 max-1060:grid-cols-2 max-md:justify-items-center max-md:gap-x-4">
                 @elseif($count_products >= 3)
-                    <div class="grid grid-cols-2 gap-6 max-1060:grid-cols-2 max-md:justify-items-center max-md:gap-x-4">
+                    <div class="grid grid-cols-3 gap-6 max-1060:grid-cols-2 max-md:justify-items-center max-md:gap-x-4">
                 @endif
                 @foreach($artistes->products as $product)
                         @foreach ($product->images as $image)
@@ -78,7 +78,7 @@
                     @endforeach
                 </div>
             </div>
-            @if($count_products === 2)
+            @if($count_products <= 2)
                 <div class="col-span-3">
             @elseif($count_products >= 3)
                 <div class="col-span-2">
@@ -97,9 +97,9 @@
                         }
                     }
                 @endphp
-                @if($count_products === 2)
+                @if($count_products <= 2)
                     <div class="grid grid-cols-2 gap-6 max-1060:grid-cols-1 max-md:gap-x-4">
-                @elseif($count_products === 3)
+                @elseif($count_products >= 3)
                     <div class="grid grid-cols-1 gap-6 max-1060:grid-cols-1 max-md:gap-x-4">
                 @endif
                     @php
