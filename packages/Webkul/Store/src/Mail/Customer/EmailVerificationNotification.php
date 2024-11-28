7f44b0c5bc8fe13094fbca18a59111629b6f311a
c4ps-lock3d <nicolas.perrenoud@gmail.com>
2024-11-28 12:54:45 +0100

    update imagecache.php and EmailVerificationNotification.php

--- packages/Webkul/Store/src/Mail/Customer/EmailVerificationNotification.php
+++ packages/Webkul/Store/src/Mail/Customer/EmailVerificationNotification.php
@@ -24,10 +24,7 @@ class EmailVerificationNotification extends Mailable
     {
         return new Envelope(
             to: [
-                new Address(
-                    core()->getAdminEmailDetails()['email'],
-                    core()->getAdminEmailDetails()['name']
-                ),
+                new Address($this->customer->email),
             ],
             subject: trans('shop::app.emails.customers.verification.subject'),
         );
