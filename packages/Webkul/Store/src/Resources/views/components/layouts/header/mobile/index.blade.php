f1fdde65c9d6bd4da1bf2432c458f41f0c2871da
c4ps-lock3d <nicolas.perrenoud@gmail.com>
2024-12-02 21:35:08 +0100

    update imagecache.php and packages/Webkul/Store/src/Resources/views/components/layouts/header/mobile/index.blade.php

--- packages/Webkul/Store/src/Resources/views/components/layouts/header/mobile/index.blade.php
+++ packages/Webkul/Store/src/Resources/views/components/layouts/header/mobile/index.blade.php
@@ -263,7 +263,7 @@
     {!! view_render_event('bagisto.shop.components.layouts.header.mobile.search.before') !!}
 
     <!-- Serach Catalog Form -->
-    <form action="{{ route('shop.search.index') }}" class="flex w-full mt-5 items-center">
+    <form action="{{ route('shop.search.index') }}" class="flex w-full mt-[-5px] items-center">
         <label 
             for="organic-search" 
             class="sr-only"
@@ -276,7 +276,7 @@
 
             <input
                 type="text"
-                class="block w-full rounded-xl border border-['#E3E3E3'] px-11 py-3.5 text-sm font-medium text-gray-900 max-md:rounded-lg max-md:px-10 max-md:py-3 max-md:font-normal max-sm:text-xs"
+                class="block w-full rounded-xl bg-white/85 placeholder-[#1d2124] border border-['#000000'] px-11 py-3.5 text-sm font-medium text-[#1d2124] max-md:rounded-lg max-md:px-10 max-md:py-3 max-md:font-normal max-sm:text-xs"
                 name="query"
                 value="{{ request('query') }}"
                 placeholder="@lang('shop::app.components.layouts.header.search-text')"
