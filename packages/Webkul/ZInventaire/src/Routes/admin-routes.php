f1f5e31f4e2a68a61dd0e64ac952f3f7ecbe4b6d
c4ps-lock3d <nicolas.perrenoud@gmail.com>
2025-01-06 13:20:33 +0100

    update 7 files

--- packages/Webkul/ZInventaire/src/Routes/admin-routes.php
+++ packages/Webkul/ZInventaire/src/Routes/admin-routes.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Webkul\ZInventaire\Http\Controllers\Admin\ZInventaireController;
 use Webkul\Store\Http\Controllers\API\ProductController;
+use Webkul\Admin\Http\Controllers\Catalog\ProductController as AdminProductController;
 
 Route::group(['middleware' => ['web', 'admin'], 'prefix' => 'admin/zinventaire'], function () {
     Route::controller(ZInventaireController::class)->group(function () {
@@ -11,4 +12,8 @@ Route::group(['middleware' => ['web', 'admin'], 'prefix' => 'admin/zinventaire']
     Route::controller(ProductController::class)->prefix('products')->group(function () {
         Route::get('inv', 'inventaire');
     });
+});
+
+Route::controller(AdminProductController::class)->prefix('products')->group(function () {
+    Route::put('edit/{id}', 'update')->name('admin.catalog.products.update');
 });
