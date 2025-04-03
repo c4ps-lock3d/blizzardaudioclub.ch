fcb0dcb78a6104b34679140e97bd97a8dd93f2df
c4ps-lock3d <nicolas.perrenoud@gmail.com>
2025-04-03 18:18:56 +0200

    update imagecache.php and location-sono.blade.php

--- packages/Webkul/Store/src/Resources/views/home/location-sono.blade.php
+++ packages/Webkul/Store/src/Resources/views/home/location-sono.blade.php
@@ -12,16 +12,11 @@
     <div class="container px-[60px] max-lg:px-8 max-md:px-4">
         <p class="mt-6 text-justify">
             Le label Blizzard Audio Club a acquis du matériel de sonorisation afin d’organiser ses propres événements, mais également de le mettre en location.<br><br>
-            Il est adapté pour des événements intérieurs et extérieurs (sous protection des intempéries) et permet de sonoriser :<br><br>
+            Il est adapté pour des événements intérieurs et extérieurs (sous protection des intempéries) et est composé d'un sub EAW RS118, de deux haut-parleurs QSC KW-Series KW153 avec pieds standards K-M 21460 et de tous les câbles nécessaires (le tout est détaillé dans nos <a class="!text-[#FADA00]" href="{{url('/images/sono/bac-soundsystem-renting-cg.pdf')}}" target="_blank">conditions générales</a>)<br><br>
         </p>
-            <ul class="list-disc">
-                <li class="ml-8">Des lives (matériel DJ non-fourni)</li>
-                <li class="ml-8">Des dj-sets (matériel DJ non-fourni)</li>
-                <li class="ml-8">Des concerts (instruments non-fournis)</li>
-            </ul><br>
         <p class="text-justify">
-            Le matériel est loué sur une base journalière. Des arrangements peuvent néanmoins être convenus, tels que des demi-journées ou des durées plus longues (Le tarif sera adapté selon). La location à l’heure n’est pas proposée.<br><br>
-            Le tarif de base de la location s’élève à 200 CHF [DEUX-CENTS FRANCS] par jour. La moitié de ce montant est à verser avant la prise du matériel. L’autre moitié au retour du matériel. Les éventuels frais de réparation ou de remplacement de matériel y seront ajoutés (voir les <a class="!text-[#FADA00]" href="{{url('/images/sono/bac-soundsystem-renting-cg.pdf')}}" target="_blank">conditions générales</a>).<br><br>
+            Le matériel est loué sur une base journalière. Des arrangements peuvent néanmoins être convenus, tels que des demi-journées ou des durées plus longues (le tarif sera adapté selon). La location à l’heure n’est pas proposée.<br><br>
+            Le tarif de base de la location s’élève à 200 CHF [DEUX-CENTS FRANCS] par jour. La moitié de ce montant est à verser avant la prise du matériel. L’autre moitié au retour du matériel. Les éventuels frais de réparation ou de remplacement de matériel y seront ajoutés.<br><br>
         </p>
         <p>Intéressé(e) ? <a class="!text-[#FADA00]" href="{{ route('shop.home.contact_us') }}">contactez-nous via notre formulaire</a>.</p>
         <div class="mt-10 grid grid-cols-2 gap-6">
